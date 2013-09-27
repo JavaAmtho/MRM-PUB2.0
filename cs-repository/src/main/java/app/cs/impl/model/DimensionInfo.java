21e9331ef6e393e7b49ab94cda0e168674ef94e3
amarpotghan <amarpotghan@gmail.com>
2013-09-27 20:01:48 +0530

    changing image support

--- cs-repository/src/main/java/app/cs/impl/model/DimensionInfo.java
+++ cs-repository/src/main/java/app/cs/impl/model/DimensionInfo.java
@@ -2,67 +2,94 @@ package app.cs.impl.model;
 
 public class DimensionInfo {
 
-	private String name;
-	private String managerName;
-	private String startDate;
-	private String endDate;
+	private String actualImage;
+	private String budget;
 	private String budgetOwner;
 	private String currency;
-	private String budget;
+	private String endDate;
+	private String managerName;
+	private String name;
+	private String previewImage;
+	private String previewType;
+	private String startDate;
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getManagerName() {
-		return managerName;
-	}
-
-	public void setManagerName(String managerName) {
-		this.managerName = managerName;
-	}
-
-	public String getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(String startDate) {
-		this.startDate = startDate;
-	}
-
-	public String getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(String endDate) {
-		this.endDate = endDate;
-	}
-
-	public String getBudgetOwner() {
-		return budgetOwner;
-	}
-
-	public void setBudgetOwner(String budgetOwner) {
-		this.budgetOwner = budgetOwner;
-	}
-
-	public String getCurrency() {
-		return currency;
-	}
-
-	public void setCurrency(String currency) {
-		this.currency = currency;
+	public String getActualImage() {
+		return actualImage;
 	}
 
 	public String getBudget() {
 		return budget;
 	}
 
+	public String getBudgetOwner() {
+		return budgetOwner;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public String getManagerName() {
+		return managerName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPreviewImage() {
+		return previewImage;
+	}
+
+	public String getPreviewType() {
+		return previewType;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setActualImage(String actualImage) {
+		this.actualImage = actualImage;
+	}
+
 	public void setBudget(String amount) {
 		this.budget = amount;
+	}
+
+	public void setBudgetOwner(String budgetOwner) {
+		this.budgetOwner = budgetOwner;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPreviewImage(String previewImage) {
+		this.previewImage = previewImage;
+	}
+
+	public void setPreviewType(String previewType) {
+		this.previewType = previewType;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
 	}
 }
--- cs-repository/src/main/java/app/cs/impl/model/MultiDimensionalObject.java
+++ cs-repository/src/main/java/app/cs/impl/model/MultiDimensionalObject.java
@@ -12,156 +12,45 @@ import com.cs.data.api.core.GenericDomain;
 @Component
 public class MultiDimensionalObject implements Serializable, GenericDomain {
 
-	public String getPreviewImage() {
-		return previewImage;
-	}
-
-	public void setPreviewImage(String previewImage) {
-		this.previewImage = previewImage;
-	}
-
-	public String getActualImage() {
-		return actualImage;
-	}
-
-	public void setActualImage(String actualImage) {
-		this.actualImage = actualImage;
-	}
-
-	public String getPreviewType() {
-		return previewType;
-	}
-
-	public void setPreviewType(String previewType) {
-		this.previewType = previewType;
-	}
-
 	private static final long serialVersionUID = 1L;
-	private final String PAGE = "page";
-	private String id;
-	private String type;
-	private String path;
-	private String name;
-	private String title;
-	private String previewImage;
-	private String actualImage;
-	private String previewType;
-	private boolean isFolder;
-	private String manager;
-	private String startDate;
-	private String endDate;
-	private String budgetOwner;
-	private String currency;
-	private String budget;
-
-	public String getManager() {
-		return dimensionInfo.getManagerName();
-	}
-
-	public String getStartDate() {
-		return dimensionInfo.getStartDate();
-	}
-
-	public String getEndDate() {
-		return dimensionInfo.getEndDate();
-	}
-
-	public String getBudgetOwner() {
-		return dimensionInfo.getBudgetOwner();
-	}
-
-	public String getCurrency() {
-		return dimensionInfo.getCurrency();
-	}
-
-	public String getBudget() {
-		return dimensionInfo.getBudget();
-	}
-
-	public void setBudget(String budget) {
-		this.budget = budget;
-	}
 
 	private List<Assortment> assortments;
-	private List<MultiDimensionalObject> items;
 
-	@JsonProperty("Items")
-	public List<MultiDimensionalObject> getItems() {
-		return children;
-	}
+	private String budget;
+
+	private String budgetOwner;
 
 	private List<MultiDimensionalObject> children;
 
-	private List<Product> products;
-
+	private String currency;
 	private DimensionInfo dimensionInfo;
+	private String endDate;
+	private List<String> groupIds;
+	private String id;
+	private String image;
+	private boolean isFolder;
+	private List<MultiDimensionalObject> items;
+	private String manager;
+	private String name;
+	private final String PAGE = "page";
+	private String path;
+	private List<Product> products;
+	private String startDate;
+	private String title;
 
-	public DimensionInfo getDimensionInfo() {
-		return dimensionInfo;
-	}
-
-	public void setDimensionInfo(DimensionInfo dimensionInfo) {
-		this.dimensionInfo = dimensionInfo;
-	}
+	private String type;
 
 	public MultiDimensionalObject() {
 	}
 
-	public List<Assortment> getAssortments() {
-		return assortments;
-	}
-
-	public void setAssortments(List<Assortment> assortments) {
-		this.assortments = assortments;
-	}
-
-	public List<Product> getProducts() {
-		return products;
-	}
-
-	public void setProducts(List<Product> products) {
-		this.products = products;
-	}
-
-	public boolean getIsFolder() {
-		return isFolder;
-	}
-
-	public void setIsFolder(boolean isFolder) {
-		this.isFolder = isFolder;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	/** The group ids. */
-	private List<String> groupIds;
-	private String image;
-
-	public List<Assortment> getAssortment() {
-		return assortments;
-	}
-
-	public void setAssortment(List<Assortment> assortment) {
-		this.assortments = assortment;
-	}
-
-	public List<String> getGroupId() {
-		return groupIds;
-	}
-
-	public MultiDimensionalObject(String id, String type, String title,
-			String name, String path) {
-		this.id = id;
-		this.type = type;
+	public MultiDimensionalObject(String name, String type, String path,
+			boolean isFolder) {
+		this.id = name;
 		this.name = name;
-		this.title = title;
+		this.title = name;
+		this.type = type;
 		this.path = path;
+		this.isFolder = isFolder;
 	}
 
 	public MultiDimensionalObject(String id, String type, String path,
@@ -176,110 +65,13 @@ public class MultiDimensionalObject implements Serializable, GenericDomain {
 		this.children = children;
 	}
 
-	public void setGroupId(List<String> groupId) {
-		this.groupIds = groupId;
-	}
-
-	public List<MultiDimensionalObject> getChildren() {
-		return children;
-	}
-
-	public void setChildren(List<MultiDimensionalObject> children) {
-		this.children = children;
-	}
-
-	public MultiDimensionalObject(String name, String type, String path,
-			boolean isFolder) {
-		this.id = name;
-		this.name = name;
-		this.title = name;
-		this.type = type;
-		this.path = path;
-		this.isFolder = isFolder;
-	}
-
-	public String getPath() {
-		return path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
+	public MultiDimensionalObject(String id, String type, String title,
+			String name, String path) {
 		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
+		this.type = type;
 		this.name = name;
-	}
-
-	@Override
-	public String getObjectKey() {
-		return "DIMENSION";
-	}
-
-	@Override
-	public String getKey() {
-		return getId();
-	}
-
-	@Override
-	public String toString() {
-		return "DimensionModel [id=" + id + ", type=" + type + ", path=" + path
-				+ ", title=" + title + ", name=" + name + ", groupIds="
-				+ groupIds + ", children=" + children + "]";
-	}
-
-	public boolean isRoot() {
-		return path == "-1" ? true : false;
-	}
-
-	public void addToGroupId(String groupId) {
-		if (groupIds == null) {
-			groupIds = new ArrayList<String>();
-			groupIds.add(groupId);
-		} else {
-
-			groupIds.add(groupId);
-		}
-
-	}
-
-	public boolean hasChildren() {
-		return this.children == null ? false : true;
-	}
-
-	public boolean isPage() {
-		return type == PAGE ? true : false;
-	}
-
-	public void addchild(MultiDimensionalObject contentObject) {
-		List<MultiDimensionalObject> newChildren;
-		if (this.children == null) {
-			newChildren = new ArrayList<MultiDimensionalObject>();
-			newChildren.add(contentObject);
-			this.children = newChildren;
-
-		} else {
-			this.children.add(contentObject);
-		}
+		this.title = title;
+		this.path = path;
 	}
 
 	public void addAssortment(Assortment assortment) {
@@ -294,11 +86,152 @@ public class MultiDimensionalObject implements Serializable, GenericDomain {
 		}
 	}
 
-	public void removeChild(MultiDimensionalObject contentObject) {
-		if (children != null) {
-			this.children.remove(contentObject);
+	public void addchild(MultiDimensionalObject contentObject) {
+		List<MultiDimensionalObject> newChildren;
+		if (this.children == null) {
+			newChildren = new ArrayList<MultiDimensionalObject>();
+			newChildren.add(contentObject);
+			this.children = newChildren;
+
+		} else {
+			this.children.add(contentObject);
+		}
+	}
+
+	public void addToGroupId(String groupId) {
+		if (groupIds == null) {
+			groupIds = new ArrayList<String>();
+			groupIds.add(groupId);
+		} else {
+
+			groupIds.add(groupId);
 		}
 
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+
+		MultiDimensionalObject other = (MultiDimensionalObject) obj;
+		if (id.equals(other.id))
+			return true;
+		else
+			return false;
+
+	}
+
+	@JsonProperty("previewImage")
+	public String getPreviewImage() {
+		return dimensionInfo.getPreviewImage();
+	}
+
+	@JsonProperty("previewType")
+	public String getPreviewType() {
+		return dimensionInfo.getActualImage();
+	}
+
+	@JsonProperty("actualImage")
+	public String getActualImage() {
+		return dimensionInfo.getActualImage();
+	}
+
+	public List<Assortment> getAssortment() {
+		return assortments;
+	}
+
+	public List<Assortment> getAssortments() {
+		return assortments;
+	}
+
+	public String getBudget() {
+		return dimensionInfo.getBudget();
+	}
+
+	public String getBudgetOwner() {
+		return dimensionInfo.getBudgetOwner();
+	}
+
+	public List<MultiDimensionalObject> getChildren() {
+		return children;
+	}
+
+	public String getCurrency() {
+		return dimensionInfo.getCurrency();
+	}
+
+	public DimensionInfo getDimensionInfo() {
+		return dimensionInfo;
+	}
+
+	public String getEndDate() {
+		return dimensionInfo.getEndDate();
+	}
+
+	public List<String> getGroupId() {
+		return groupIds;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getImageUrl() {
+		return image;
+	}
+
+	public boolean getIsFolder() {
+		return isFolder;
+	}
+
+	@JsonProperty("Items")
+	public List<MultiDimensionalObject> getItems() {
+		return children;
+	}
+
+	@Override
+	public String getKey() {
+		return getId();
+	}
+
+	public String getManager() {
+		return dimensionInfo.getManagerName();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getObjectKey() {
+		return "DIMENSION";
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public String getStartDate() {
+		return dimensionInfo.getStartDate();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public boolean hasChildren() {
+		return this.children == null ? false : true;
 	}
 
 	@Override
@@ -319,27 +252,82 @@ public class MultiDimensionalObject implements Serializable, GenericDomain {
 		return result;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
+	public boolean isPage() {
+		return type == PAGE ? true : false;
+	}
 
-		MultiDimensionalObject other = (MultiDimensionalObject) obj;
-		if (id.equals(other.id))
-			return true;
-		else
-			return false;
+	public boolean isRoot() {
+		return path == "-1" ? true : false;
+	}
+
+	public void removeChild(MultiDimensionalObject contentObject) {
+		if (children != null) {
+			this.children.remove(contentObject);
+		}
 
 	}
 
-	public String getImageUrl() {
-		return image;
+	public void setAssortment(List<Assortment> assortment) {
+		this.assortments = assortment;
+	}
+
+	public void setAssortments(List<Assortment> assortments) {
+		this.assortments = assortments;
+	}
+
+	public void setBudget(String budget) {
+		this.budget = budget;
+	}
+
+	public void setChildren(List<MultiDimensionalObject> children) {
+		this.children = children;
+	}
+
+	public void setDimensionInfo(DimensionInfo dimensionInfo) {
+		this.dimensionInfo = dimensionInfo;
+	}
+
+	public void setGroupId(List<String> groupId) {
+		this.groupIds = groupId;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public void setImage(String image) {
 		this.image = image;
 
+	}
+
+	public void setIsFolder(boolean isFolder) {
+		this.isFolder = isFolder;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "DimensionModel [id=" + id + ", type=" + type + ", path=" + path
+				+ ", title=" + title + ", name=" + name + ", groupIds="
+				+ groupIds + ", children=" + children + "]";
 	}
 }
